454894f336de20e20f029c6c431c3b3f08330aac
John Tom Jr <johntjr.22@gmail.com>
2020-03-14 04:47:15 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,5 +1,12 @@
 def nyc_pigeon_organizer(data)
 new_hash = {}
   data.each do |key, value|
+    value.each do |new_value, names|
+      names.each do |name| 
+        
+  
+        
+        end
+    end
 end
 end
